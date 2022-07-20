346b1b5d8a0fcc66fc4a2cb377fceef35e4598fc
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-20 16:12:59 -0400

    retrieve products from mongoDB

--- mongo.js
+++ mongo.js
@@ -1,28 +1,43 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
 const url =
-  'mongodb+srv://anthony:QeAOmiKUj2AEKDvn@cluster0.p3dbegd.mongodb.net/?retryWrites=true&w=majority'
+  "mongodb+srv://anthony:QeAOmiKUj2AEKDvn@cluster0.p3dbegd.mongodb.net/?retryWrites=true&w=majority";
 
 const createProduct = async (req, res, next) => {
   const newProduct = {
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
   };
   const client = new MongoClient(url);
 
   try {
     await client.connect();
     const db = client.db("products_test");
-    const result = db.collection('products').insertOne(newProduct);
+    const result = db.collection("products").insertOne(newProduct);
   } catch (error) {
-    return res.json({message: 'Could not store data.'});
-  };
+    return res.json({ message: "Could not store data." });
+  }
   client.close();
 
   res.json(newProduct);
 };
 
-const getProducts = async (req, res, next) => {};
+const getProducts = async (req, res, next) => {
+  const client = new MongoClient(url);
+
+  let products;
+
+  try {
+    await client.connect();
+    const db = client.db("products_test");
+    products = await db.collection("products").find().toArray();
+  } catch (error) {
+    return res.json({ message: "Could not get data." });
+  }
+  client.close();
+
+  res.json(products);
+};
 
 exports.createProduct = createProduct;
 exports.getProducts = getProducts;
