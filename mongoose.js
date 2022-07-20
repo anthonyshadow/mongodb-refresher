6b21ece1974df08532517688aa4db29df54b5785
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-20 16:41:15 -0400

    post products to database using mongoose

--- mongoose.js
+++ mongoose.js
@@ -1,12 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const Product = require('./models/product')
+const Product = require("./models/product");
 
-const createProduct = async( req, res, next) => {
-    const createdProduct = new Product({
-        name: req.body.name,
-        price: req.body.price
-    });
-}
+const url =
+  "mongodb+srv://anthony:QeAOmiKUj2AEKDvn@cluster0.p3dbegd.mongodb.net/products_test?retryWrites=true&w=majority";
 
-exports.createProduct = createProduct
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connect to database");
+  })
+  .catch(() => {
+    console.log("connection failed")
+  });
+
+const createProduct = async (req, res, next) => {
+  const createdProduct = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  });
+  const result = await createdProduct.save();
+
+  res.json(result);
+};
+
+exports.createProduct = createProduct;
